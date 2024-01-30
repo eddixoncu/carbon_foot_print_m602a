7c2d36496ba442548c5fb5b31c19200bd1da4607
Eddixon <eddx.castillo@gmail.com>
2024-01-30 12:30:21 +0100

    Practicing with operators

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
 msg = "Roll the dice"
 print(msg)
+m = 10 * 5
+print("result is", m)
+m = 6 - 3
+print("result is", m)
+m = 68.75 * 0
+print("result is", m)
+m = 100 / 17
+print("result is", m)
+m = 100 % 17
+print("result is", m)
+
+zero = 0.0
+try:
+    m = 100 / zero
+    print("result is", m)
+except Exception as e:
+    print('Something wrong happened', e)
+
+m = 2 ** 3
+print("result is", m)
+
+x = 5
+y = 3
+print("result is", x * y)
