847fa7c3833712bc2ef5302f319da1f26a0458cb
Eddixon <eddx.castillo@gmail.com>
2024-02-13 13:04:08 +0100

    validating user's input

--- main.py
+++ main.py
@@ -1,21 +1,34 @@
-Temperature = 28
+name = input("Enter name: ")
+lower_name = name.lower()
+
+if lower_name == 'james bond':
+    print("We've been expecting you, Mr bond")
+else:
+    print("Access Denied: ", name)
+
+# value = input("Enter an integer or float value: ")
+
+# if value.isdigit():
+#     print("Value is integer")
+# elif value.isdecimal():
+#     print("value is float")
+# else:
+#     print("is another else")
 
 
-def thermostat_regulator(temperature, low_temp=20, high_temp=27):
-    # assert high_temp > low_temp, "High temp must be lower than low temp"
-    state = ''
-    if temperature < low_temp:
-        state = 'Turn on heater'
-    elif temperature > high_temp:
-        state = 'Turn on cooler'
-    # elif low_temp <= temperature <= high_temp:
-    else:
-        state = 'Do nothing'
-
-    # print(state)
-    return state
-
-
-result = thermostat_regulator(Temperature, 1500, 2700)
-
-print('result is ', result)
+user_input = input("Enter an integer or float value: ")
+print(type(user_input) )
+print("is num ", user_input.isnumeric())
+print("is dec ", user_input.isdecimal())
+print("is alf ", user_input.isalpha())
+print("is alf /num ", user_input.isalnum() )
+'''
+value = eval (user_input)
+print (type(value))
+if type(value) == int:
+    print("Value is integer")
+elif type(value) == float:
+    print("Value es float")
+else:
+    print("IS another else")
+'''
