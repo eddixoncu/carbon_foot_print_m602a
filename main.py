16677e9e8453a21b3440097610e1e28913fb80c1
Eddixon <eddx.castillo@gmail.com>
2024-02-20 12:39:03 +0100

    Code for the session nunmber 6

--- main.py
+++ main.py
@@ -1,34 +1,81 @@
-name = input("Enter name: ")
-lower_name = name.lower()
-
-if lower_name == 'james bond':
-    print("We've been expecting you, Mr bond")
-else:
-    print("Access Denied: ", name)
-
-# value = input("Enter an integer or float value: ")
-
-# if value.isdigit():
-#     print("Value is integer")
-# elif value.isdecimal():
-#     print("value is float")
-# else:
-#     print("is another else")
+"""
+def greeting(name):
+    return 'Goodbye ' + name
 
 
-user_input = input("Enter an integer or float value: ")
-print(type(user_input) )
-print("is num ", user_input.isnumeric())
-print("is dec ", user_input.isdecimal())
-print("is alf ", user_input.isalpha())
-print("is alf /num ", user_input.isalnum() )
-'''
-value = eval (user_input)
-print (type(value))
-if type(value) == int:
-    print("Value is integer")
-elif type(value) == float:
-    print("Value es float")
-else:
-    print("IS another else")
-'''
+def greeting(name):
+    return 'HEllo ' + name
+
+
+print(greeting('data'))
+"""
+
+
+def square(number):
+    return number * number
+
+
+squared = square(9)
+print("Square is ", squared)
+
+
+def max_num(number1, number2):
+    if number1 >= number2:
+        return number1
+    else:
+        return number2
+
+
+maximum = max_num(50, 50)
+print("Max is ", maximum)
+maximum = max_num(51, 50)
+print("Max is ", maximum)
+maximum = max_num(50, 52)
+print("Max is ", maximum)
+
+
+def calculate_tax(salary):
+    if salary <= 20000:
+        return 0
+    elif 20000 < salary <= 50000:
+        return salary * 0.1
+    elif 50000 < salary <= 100000:
+        return salary * 0.2
+    else:
+        return salary * 0.3
+
+
+calculated_tax = calculate_tax(20000)
+print('Calculated tax is ', calculated_tax)
+
+calculated_tax = calculate_tax(20001)
+print('Calculated tax is ', calculated_tax)
+calculated_tax = calculate_tax(67000)
+print('Calculated tax is ', calculated_tax)
+
+calculated_tax = calculate_tax(130000)
+print('Calculated tax is ', calculated_tax)
+
+
+def reverse_string(string: str):
+    # a slice that steps backwards, -1.
+    return string[::-1]
+
+
+txt = "AnimaL"
+print('Reversed is', reverse_string(txt))
+
+
+def average(numbers):
+    divisor = len(numbers)
+    accumulated = 0
+    for n in numbers:
+        accumulated += n
+
+    return accumulated / divisor
+
+
+list_numbers = [5, 2, 6, 8, 7]
+avg = average(list_numbers)
+print("average is ", avg)
+
