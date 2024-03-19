21a9fed6c7e579bd25f765f669830946b9d88a0b
Eddixon <eddx.castillo@gmail.com>
2024-03-19 11:33:38 +0100

    String operations

--- main.py
+++ main.py
@@ -1,10 +1,44 @@
-import warmUpModule
-from game.enemy import enemies
+
+#Counting characters
+def count_chars (string, char):
+    num = string.count(char)
+    print (f"Number of occurrences of {char} is {num}")
 
 
-area = warmUpModule.calculate_random_area()
-print(f"random area is {area}")
+#Checking substrings
+def checking_substring ( string, substring):
+    exist = substring in string
+    print(f"Is the substring {substring} exist in {string}?\n : {exist}")
 
-warmUpModule.print_current_date()
 
-enemies.print_enemies()
+def to_low(string:str):
+    return string.lower()
+
+def to_up(string:str):
+    return string.upper()
+
+def to_split(string:str, char):
+    parts = string.split(char)
+    return  parts
+
+def to_unsplit(parts:[], separator):
+    string = separator.join(parts)
+    return string
+
+
+def del_trail_white_space(string:str):
+    return string.strip()
+
+
+string = input("enter the input string >>: ")
+substring = input("enter the character to search >>: ")
+print("counting chars ", count_chars( string, substring))
+print(f"is {substring} in {string}? : {checking_substring(string,substring)}")
+print(to_low(string))
+print(to_up(string))
+tokens = to_split(string, substring)
+print(tokens)
+print ( to_unsplit(tokens, " & "))
+print(del_trail_white_space(string))
+
+#Un diablo se cayó al  agua Y otro diablo lo sacó Y otro diablo que pasaba Dijo: qué diablos pasó ? El diablito engarrotado dijo: Qué voy a saber A lo mejor fue la diabla La diabla de mi mujer
